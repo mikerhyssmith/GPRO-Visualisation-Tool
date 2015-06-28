cdaad5f89f14fad5d25b56101f60d1916453883b
Josh <joshdt001@gmail.com>
2015-06-28 23:29:38 +0100

    Memory management

--- GPRO-Visualisation-Tool/src/analysis/DataSet.java
+++ GPRO-Visualisation-Tool/src/analysis/DataSet.java
@@ -9,24 +9,27 @@ import data.User;
 
 public class DataSet {
     
+    @SuppressWarnings("unchecked")
     public static ArrayList<Race> getRaces()
     {
         return (ArrayList<Race>) Data.getInstance().races().clone();
     }
     
+    @SuppressWarnings("unchecked")
     public static ArrayList<RaceTrack> getTracks()
     {
-        return (ArrayList<RaceTrack>) Data.getInstance().raceTracks().values();
+        return (ArrayList<RaceTrack>) ((ArrayList<RaceTrack>) Data.getInstance().raceTracks().values()).clone();
     }
     
     @SuppressWarnings("unchecked")
     public static ArrayList<String> getTrackNames()
     {
-        return (ArrayList<String>) Data.getInstance().raceTracks().keySet();
+        return (ArrayList<String>) ((ArrayList<String>) Data.getInstance().raceTracks().keySet()).clone();
     }
     
+    @SuppressWarnings("unchecked")
     public static ArrayList<User> getUsers()
     {
-        return Data.getInstance().users();
+        return (ArrayList<User>) Data.getInstance().users().clone();
     }
 }
